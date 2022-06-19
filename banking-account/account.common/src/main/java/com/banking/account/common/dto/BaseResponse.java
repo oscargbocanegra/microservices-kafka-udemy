4f3cdd8432757ab2ed9a913ef6c8ab457211f60c
platziadmin <platziadmin@master.localdomain>
2022-06-19 15:30:54 -0500

    normalizacion proyecto

---
+++ banking-account/account.common/src/main/java/com/banking/account/common/dto/BaseResponse.java
@@ -0,0 +1,15 @@
+package com.banking.account.common.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseResponse {
+    private String message;
+}
